4289d132105d8a6814aff2f84a7de13637e3c754
Dominic Sisneros <dsisnero@gmail.com>
2016-01-29 08:48:19 -0700

    added ffmpeg

--- modules/people/manifests/dsisnero/applications.pp
+++ modules/people/manifests/dsisnero/applications.pp
@@ -1,6 +1,18 @@
 class people::dsisnero::applications {
 
- 
+  include homebrew
 
-    
+  package { 'ffmpeg':
+    ensure => present,
+    install_options =>[
+                       '--with-fdk-aac',
+                       '--with-libass',
+                       '--with-x265'
+                       ]
+  }
+  
+
+  
+
+  
   }
